e32e7f720709e2c7d949afbfb60439f188505df0
anlarkn <anilarkin0@gmail.com>
2019-10-08 16:23:13 +0300

    Updates

---
+++ precache-manifest.26cc16b2d77b0541ab540d37206d10dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "150f94dccaa7674aaa3cf56b51b460a0",
+    "url": "/monsters-relodex/index.html"
+  },
+  {
+    "revision": "30c9ecd12a24785bfc06",
+    "url": "/monsters-relodex/static/css/main.bdd17646.chunk.css"
+  },
+  {
+    "revision": "7a502c8e0ee03c0cb7a5",
+    "url": "/monsters-relodex/static/js/2.04f62d5f.chunk.js"
+  },
+  {
+    "revision": "30c9ecd12a24785bfc06",
+    "url": "/monsters-relodex/static/js/main.7b329e2e.chunk.js"
+  },
+  {
+    "revision": "d13ae556cec84af04e16",
+    "url": "/monsters-relodex/static/js/runtime-main.cd5de3e0.js"
+  }
+]);
